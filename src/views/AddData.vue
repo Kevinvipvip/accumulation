b7041e95932991ec4b67700affcf97781f18527b
Kevin <grj@bwg.art>
2019-10-25 18:35:35 +0800

    添加数据到Bmob功能实现，部分地图

--- src/views/AddData.vue
+++ src/views/AddData.vue
@@ -1,41 +1,55 @@
 <template>
   <div class="add">
+    <div class="list">
+      <ul>
+        <li v-for="(v,index) in list_data" :key="index">
+          <p>{{v.id}}&nbsp;&nbsp;{{v.official_name}}&nbsp;&nbsp;&nbsp;&nbsp;<span style="color: #ff4c4c;">{{v.grade}}</span><br>&nbsp;&nbsp;&nbsp;&nbsp;功{{v.merit}}&nbsp;德{{v.virtue}}&nbsp;才{{v.ability}}&nbsp;脏{{v.dirty}}
+          </p>
+        </li>
+      </ul>
+    </div>
     <div class="ipt-box">
       <div class="ipt-cont">
         <label>官名</label>
         <div class="input">
-          <input type="text" v-model="official_name" placeholder="官名">
+          <input type="text" v-model="official_name" placeholder="官名" @blur="verify_input()"/>
         </div>
+        <p>{{tip1}}</p>
       </div>
       <div class="ipt-cont">
         <label>品级</label>
         <div class="input">
-          <input type="text" v-model="grade"/>
+          <input type="text" v-model="grade" @blur="verify_input()"/>
         </div>
+        <p>{{tip2}}</p>
       </div>
       <div class="ipt-cont">
         <label>投到“功”时升官到</label>
         <div class="input">
-          <input type="text" v-model="merit"/>
+          <input type="text" v-model="merit" @blur="verify_input()"/>
         </div>
+        <p>{{tip3}}</p>
       </div>
       <div class="ipt-cont">
         <label>投到“德”时升官到</label>
         <div class="input">
-          <input type="text" v-model="virtue"/>
+          <input type="text" v-model="virtue" @blur="verify_input()"/>
         </div>
+        <p>{{tip4}}</p>
       </div>
       <div class="ipt-cont">
         <label>投到“才”时升官到</label>
         <div class="input">
-          <input type="text" v-model="ability"/>
+          <input type="text" v-model="ability" @blur="verify_input()"/>
         </div>
+        <p>{{tip5}}</p>
       </div>
       <div class="ipt-cont">
         <label>投到“脏”时降级到</label>
         <div class="input">
-          <input type="text" v-model="dirty"/>
+          <input type="text" v-model="dirty" @blur="verify_input()"/>
         </div>
+        <p>{{tip6}}</p>
       </div>
 
       <div class="btn-add" @click="add_data">添加</div>
@@ -48,35 +62,134 @@
     name: 'AddData',
     data () {
       return {
+        list_data: [],
+
         official_name: '',
-        grade: '无',
+        grade: '',
         merit: '',
         virtue: '',
         ability: '',
-        dirty: ''
+        dirty: '',
+
+        tip1: '',
+        tip2: '',
+        tip3: '',
+        tip4: '',
+        tip5: '',
+        tip6: '',
+
+        verify_num: 0
       }
     },
     created () {
-
+      this.getListData()
     },
     methods: {
-      add_data () {
+      verify_input (complete) {
+        this.verify_num = 0
         if (!this.official_name.trim()) {
-
-        } else if (!this.grade.trim()) {
-
+          this.tip1 = '请先输入你需要添加的官名'
+        } else {
+          this.tip1 = ''
+          this.verify_num++
+          if (this.verify_num === 5) {
+            // this.add_data_to_Bmob()
+            if (complete) {
+              complete()
+            }
+          }
         }
+        if (!this.grade.trim()) {
+          this.tip2 = '此项不填则默认为无品级'
+        } else {
+          this.tip2 = ''
+          // this.verify_num++
+          if (this.verify_num === 5) {
+            // this.add_data_to_Bmob()
+            if (complete) {
+              complete()
+            }
+          }
+        }
+        if (!this.merit.trim()) {
+          this.tip3 = '请添加投掷到“功”时将升级到的官名'
+        } else {
+          this.tip3 = ''
+          this.verify_num++
+          if (this.verify_num === 5) {
+            // this.add_data_to_Bmob()
+            if (complete) {
+              complete()
+            }
+          }
+        }
+        if (!this.virtue.trim()) {
+          this.tip4 = '请添加投掷到“德”时将升级到的官名'
+        } else {
+          this.tip4 = ''
+          this.verify_num++
+          if (this.verify_num === 5) {
+            // this.add_data_to_Bmob()
+            if (complete) {
+              complete()
+            }
+          }
+        }
+        if (!this.ability.trim()) {
+          this.tip5 = '请添加投掷到“才”时将升级到的官名'
+        } else {
+          this.tip5 = ''
+          this.verify_num++
+          if (this.verify_num === 5) {
+            // this.add_data_to_Bmob()
+            if (complete) {
+              complete()
+            }
+          }
+        }
+        if (!this.dirty.trim()) {
+          this.tip6 = '请添加投掷到“脏”时将降级到的官名'
+        } else {
+          this.tip6 = ''
+          this.verify_num++
+          if (this.verify_num === 5) {
+            // this.add_data_to_Bmob()
+            if (complete) {
+              complete()
+            }
+          }
+        }
+      },
+      add_data_to_Bmob () {
+        // console.log(this.tip, '数组')
         console.log(this.official_name, '官名')
-        console.log(this.grade, '品级')
+        console.log(this.grade === '' ? '无品级' : this.grade, '品级')
         console.log(this.merit, '功')
         console.log(this.virtue, '德')
         console.log(this.ability, '才')
         console.log(this.dirty, '脏')
-        // this.Bmob.Query('tableName').set('name', 'Bmob').set('cover', '后端云').save().then(res => {
-        //   console.log(res)
-        // }).catch(err => {
-        //   console.log(err)
-        // })
+      },
+      add_data () {
+        this.verify_input(() => {
+          // this.add_data_to_Bmob()
+          let obj = this.Bmob.Query('zhuang_yuan_qi')
+          obj.set('official_name', this.official_name)
+          obj.set('grade', this.grade === '' ? '无品级' : this.grade)
+          obj.set('merit', this.merit)
+          obj.set('virtue', this.virtue)
+          obj.set('ability', this.ability)
+          obj.set('dirty', this.dirty)
+          obj.save().then(res => {
+            this.getListData()
+          }).catch(err => {
+            console.log(err)
+          })
+        })
+      },
+      getListData () {
+        this.Bmob.Query('zhuang_yuan_qi').find().then(res => {
+          this.list_data = res
+        })
       }
     }
   }
@@ -88,14 +201,36 @@
     margin: 0 auto;
     overflow: hidden;
     background-color: #eee;
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+
+    .list {
+      width: 50%;
+      margin: 30px 0;
+
+      ul {
+        width: 60%;
+        margin: 0 auto;
+
+        li {
+          margin-bottom: 20px;
+
+          p {
+            font-size: 20px;
+            line-height: 30px;
+          }
+        }
+      }
+    }
 
     .ipt-box {
       width: 50%;
-      margin: 30px auto;
+      margin: 30px 0;
 
       .ipt-cont {
         width: 50%;
-        margin: 0 auto 30px;
+        margin: 0 auto;
 
         label {
           line-height: 40px;
@@ -104,10 +239,11 @@
 
         .input {
           border: 1px solid #999;
-          width: 100%;
+          /*width: 100%;*/
           height: 30px;
           line-height: 30px;
           padding: 0 5px;
+          box-sizing: border-box;
 
           input {
             width: 100%;
@@ -115,8 +251,16 @@
             border: none;
             outline: none;
             background: unset;
-            font-size: 20px;
+            font-size: 16px;
+            display: block;
           }
+        }
+
+        p {
+          height: 30px;
+          line-height: 30px;
+          font-size: 12px;
+          color: #ff4c4c;
         }
       }
 
--- src/views/ZhuangYuanQi.vue
+++ src/views/ZhuangYuanQi.vue
@@ -1,7 +1,32 @@
 <template>
   <div class="zhuang-yuan-qi">
     <div class="games-map">
+      <div class="map-top">
 
+        <div class="line-one item-com">
+          <div class="list-item" v-for="v in list_data_one" :key="v.id">
+            <span>{{v.grade}}</span>
+            <h3>{{v.official_name}}</h3>
+            <p>功{{v.merit}}脏{{v.dirty}}<br>德{{v.virtue}}才{{v.ability}}</p>
+          </div>
+          <div class="title">外州衙门</div>
+          <div class="list-item" v-for="v in list_data_two" :key="v.id">
+            <span>{{v.grade}}</span>
+            <h3>{{v.official_name}}</h3>
+            <p>功{{v.merit}}脏{{v.dirty}}<br>德{{v.virtue}}才{{v.ability}}</p>
+          </div>
+          <div class="title">外府衙门</div>
+          <div class="line-tip">礼后与先送礼俱<br>同衙门小与大送</div>
+        </div>
+      </div>
+      <div class="line-one">
+        <div class="line-tip">功为白丁脏不上名<br>德为案首才为童生</div>
+        <div class="list-item" v-for="v in list_data" :key="v.id">
+          <h3>{{v.official_name}}</h3>
+          <p>功{{v.merit}}脏{{v.dirty}}<br>德{{v.virtue}}才{{v.ability}}</p>
+        </div>
+        <div class="line-tip">得仲状元者出任或<br>出任者俱送礼两份</div>
+      </div>
     </div>
     <div class="yao-sai-zi">
       <p>{{tip}}</p>
@@ -15,13 +40,26 @@
     data () {
       return {
         num: 0,
-        tip: '德'
+        tip: '德',
+        list_data: [],
+        list_data_one: [],
+        list_data_two: []
       }
     },
     created () {
       // console.log(this.$route, 'test的')
       this.Bmob.Query('zhuang_yuan_qi').find().then(res => {
-        console.log(res)
+        res.filter((item, index, arr) => {
+          if (item.id > 0 && item.id <= 15) {
+            // console.log(item)
+            this.list_data.push(item)
+          } else if (item.id > 15 && item.id <= 20) {
+            this.list_data_one.push(item)
+          } else if (item.id > 20 && item.id <= 27) {
+            this.list_data_two.push(item)
+          }
+        })
+        console.log(this.list_data)
       })
     },
     methods: {
@@ -63,6 +101,110 @@
     margin: 0 auto;
     overflow: hidden;
     background-color: #eee;
+
+    .games-map {
+      margin-top: 30px;
+      width: 1200px;
+      height: 1200px;
+      border: 1px solid #2D0E0B;
+      box-sizing: border-box;
+
+
+      .line-one {
+        height: 200px;
+        display: flex;
+        justify-content: flex-end;
+        border: 1px solid #2D0E0B;
+        box-sizing: border-box;
+
+        .line-tip, .list-item {
+          width: 5.88%;
+          border: 1px solid #2D0E0B;
+          box-sizing: border-box;
+          writing-mode: vertical-lr;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          font-size: 22px;
+        }
+
+        .list-item {
+          writing-mode: unset;
+          display: flex;
+          flex-flow: column;
+          justify-content: space-between;
+          align-items: center;
+
+          h3 {
+            width: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            border-bottom: 1px solid #2D0E0B;
+            height: 50px;
+            /*line-height: 50px;*/
+            font-size: 28px;
+            flex-shrink: 0;
+          }
+
+          p {
+            width: 100%;
+            margin-top: 5px;
+            flex-grow: 1;
+            border-top: 1px solid #2D0E0B;
+            writing-mode: vertical-lr;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-size: 20px;
+            letter-spacing: 2px;
+          }
+        }
+      }
+
+      .map-top {
+        height: 1000px;
+        width: 100%;
+        position: relative;
+
+        .item-com {
+          position: absolute;
+          bottom: 0;
+          right: 0;
+          width: 1000px;
+          height: 230px;
+          justify-content: flex-start;
+          transform: rotate(-90deg) translateX(100%);
+          transform-origin: 100% 100%;
+
+          .title {
+            writing-mode: vertical-lr;
+            font-size: 30px;
+            font-weight: bold;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 6.6666%;
+            border-bottom: 1px solid #2D0E0B;
+            box-sizing: border-box;
+          }
+
+          .list-item, .line-tip {
+            width: 6.6666%;
+
+            span {
+              font-size: 16px;
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              width: 100%;
+              height: 30px;
+              border-bottom: 1px solid #2D0E0B;
+            }
+          }
+        }
+      }
+    }
 
     .yao-sai-zi {
       background-color: #fff;
